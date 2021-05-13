d1716c64d92e8ba459bfd43c3dad14a372cbd64f
desparta05 <desparta05@gmail.com>
2021-05-13 08:41:24 -0500

    Villains page

---
+++ client/src/pages/Villains.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import villainData from "../data/villainData.json"
+import SearchField from "../components/SearchField"
+
+function Villains() {
+    const [villains, setVillains] = useState(villainData);
+
+    function handleChange() {
+
+    }
+}
+
+export default Villains;
