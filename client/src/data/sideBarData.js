201165a634a615b7adae9785e4bf6830081c33d3
desparta05 <desparta05@gmail.com>
2021-05-18 02:14:05 -0500

    Corrected syntax for icons

--- client/src/data/sideBarData.js
+++ client/src/data/sideBarData.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
+// import * as FaIcons from 'react-icons/fa';
+// import * as AiIcons from 'react-icons/ai';
 import * as RiIcons from 'react-icons/ri'; 
 import * as GiIcons from 'react-icons/gi'; 
 
@@ -8,31 +8,31 @@ export const sideBarData =[
     {
         title: 'Home',
         path: '/',
-        icon: 'RiIcons.RiHome2Fill',
+        icon: <RiIcons.RiHome2Fill />,
         cName: 'nav-text'
     },
     {
         title: 'Villains',
         path: '/villains',
-        icon: 'GiIcons.GiAnimalSkull',
+        icon: <GiIcons.GiAnimalSkull />,
         cName: 'nav-text'
     },
     {
         title: 'Heroes',
         path: '/heroes',
-        icon: 'GiIcons.GiBatMask',
+        icon: <GiIcons.GiBatMask />,
         cName: 'nav-text'
     },
     {
         title: 'PossibleThreats',
         path: '/possiblethreats',
-        icon: 'GiIcons.GiBrute',
+        icon: <GiIcons.GiBrute />,
         cName: 'nav-text'
     },
     {
         title: 'Equipment',
         path: '/equipment',
-        icon: 'GiIcons.GiApc',
+        icon: <GiIcons.GiApc />,
         cName: 'nav-text'
     },
 ]
