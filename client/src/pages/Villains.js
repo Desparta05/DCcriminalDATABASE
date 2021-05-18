2e1f9f6163dcf2c489a70f7aff19c1d57796f0d0
desparta05 <desparta05@gmail.com>
2021-05-18 02:12:07 -0500

    Removed SearchField for testing

--- client/src/pages/Villains.js
+++ client/src/pages/Villains.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import villainData from "../data/villainData.json"
-import SearchField from "../components/SearchField"
+// import SearchField from "../components/SearchField"
 
 function Villains() {
     const [villains, setVillains] = useState(villainData);
