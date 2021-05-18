d4ced04cb3058283cfa9bc5c85216c952cbb0672
desparta05 <desparta05@gmail.com>
2021-05-18 02:10:31 -0500

    Added Hero Route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require ("express").Router();
 const villainRoutes = require("./villains");
+const heroRoutes = require("./heroes");
 
 // Villain routes
 router.use("/villains", villainRoutes);
+
+// Hero routes
+router.use("/heroes", heroRoutes);
 
 module.exports = router;
