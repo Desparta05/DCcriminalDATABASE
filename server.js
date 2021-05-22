e6d15bccc8b9942bc5634b2c1647fcbc33aff9c8
desparta05 <desparta05@gmail.com>
2021-05-22 07:55:09 -0500

    Added API routes to server

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
 const path = require("path")
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
-
+const app = express();
+const apiRoutes = require("./routes/api");
 
 //Define Middleware here
 app.use (express.urlencoded({ extended: true }));
@@ -15,9 +13,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-
-//Add routes, both API and view
-app.use(routes);
 
 //Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/db_dcComics",
@@ -28,7 +23,17 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/db_dcComi
     }
 );
 
+//Add routes, both API and view
+app.use("/api", apiRoutes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
 app.listen(PORT, function() {
-    console.log(`=> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
 
