3684de07da6c0efecadc242905911c7f1ad5b383
desparta05 <desparta05@gmail.com>
2021-05-22 08:21:09 -0500

    Added API paths

---
+++ client/src/utils/API.js
@@ -0,0 +1,32 @@
+
+import axios from "axios";
+
+const API = {
+    getVillains: function (query) {
+        return axios.get("/api/villains", { params: { q: query }});
+    },
+
+    getVillain: function (id) {
+        return axios.get("/api/villains" + id);
+    },
+
+    login: function (body) {
+        return axios.post("/api/users/login", { data: body });
+    },
+
+    signUp: function (body) {
+        return axios.post("/api/users", { data: body });
+    },
+
+    logout: function () {
+        return axios.post("/api/users/logout");
+    },
+
+
+    // getUserVillains: function () {
+    //     return axios.get("/api/villains");
+    // },
+
+};
+
+export default API;
