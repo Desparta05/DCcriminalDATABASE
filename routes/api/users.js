b1f82ce4ab7659c46caa7954fb1fe250ae84c1d8
desparta05 <desparta05@gmail.com>
2021-05-22 08:24:29 -0500

    added user.js for routes

---
+++ routes/api/users.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const db = require("../../models");
+const bcrypt = require("bcrypt");
+
+router.post(`/`, async (req, res) => {
+    try {
+        req.body.data.password = await bcrypt.hash(req.body.data.password, 10);
+        const newUser = await db.User.create(req.body.data);
+        req.session.userId = newUser._id;
+        req.session.loggedIn = true;
+        res.status(200).json({ user: newUser, message: "New user created" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await db.User.findOne({ username: req.body.data.username });
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+        bcrypt.compare(req.body.data.password, userData.password, (err, isMatching) => {
+            if (err) {
+                console.log(err);
+            } else if (isMatching) {
+                req.session.userId = userData._id;
+                req.session.save(() => {
+                    req.session.loggedIn = true;
+                    res.json({ user: userData, message: 'You are now logged in!' });
+                });
+                console.log(`successful login!`)
+            } else {
+                res.status(400).json({ message: 'Incorrect email or password, please try again' });
+                return;
+            }
+        })
+    } catch (err) {
+        res.status(400).json(err);
+    };
+});
+
+// Logging out
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            console.log("session ended");
+        });
+    } else {
+        res.status(404).end();
+    };
+});
+
+module.exports = router;
