2ea0927215c01713d7779c25ee3596820e5ce81d
desparta05 <desparta05@gmail.com>
2021-05-22 08:05:00 -0500

    Created Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header({ heading }) {
+    return (
+        <div className="row d-flex justify-content-center">
+            <header className="mb-2 mx-2">
+                <h1 className="text-center">{heading}</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
