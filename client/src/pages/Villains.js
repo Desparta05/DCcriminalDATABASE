dae709735c2c4c8d1f97eed171ca9f3d3cdffdb3
desparta05 <desparta05@gmail.com>
2021-05-22 08:19:52 -0500

    Villains page changed to read the Json file.

--- client/src/pages/Villains.js
+++ client/src/pages/Villains.js
@@ -1,13 +1,55 @@
-import React, {useState, useEffect} from 'react';
-import villainData from "../data/villainData.json"
-// import SearchField from "../components/SearchField"
+import React, { Component } from 'react';
+// import Jumbotron from "../components/Jumbotron";
+// import API from "../utils/API";
+import ProfileCard from "../components/ProfileCard";
+// import { List, ListItem } from "../components/List";
+// import { Col, Row, Container } from "../components/Grid";
+import Wrapper from "../components/Wrapper";
+import Header from "../components/Header";
+// import SearchField from '../components/SearchField';
+import villains from "../villains.json"
 
-function Villains() {
-    const [villains, setVillains] = useState(villainData);
+class Villains extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    villains
+  };
 
-    function handleChange() {
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const villains = this.state.villains.filter(villain => villain._id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ villains });
+  };
 
-    }
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Header>Villains List</Header>
+        {this.state.villains.map(villain => (
+          <ProfileCard
+          key={villain._id} 
+          {...villain}
+          // id={villain.id}
+          //   alias={villain.Alias} 
+          //   name={villain.Name} 
+          //   imageSrc={villain.Image}
+          //   city={villain.City}
+          //   gender={villain.Gender}
+          //   height={villain.Height}
+          //   weight={villain.Weight}
+          //   eyeColor={villain.EyeColor}
+          //   hairColor={villain.HairColor}
+          //   powers={villain.Powers}
+          //   description={villain.Description}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default Villains;
+
+// Alias, Image, Name, City, Gender, Height, Weight, EyeColor, HairColor, Powers, Description
