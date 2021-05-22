7c7e1d6e14cdde5e80d503907ad3ae430b313a57
desparta05 <desparta05@gmail.com>
2021-05-22 08:23:22 -0500

    Added router.use for User

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,15 @@
 const router = require ("express").Router();
 const villainRoutes = require("./villains");
 const heroRoutes = require("./heroes");
+const userRoutes = require("./users")
 
 // Villain routes
 router.use("/villains", villainRoutes);
 
 // Hero routes
 router.use("/heroes", heroRoutes);
+
+// User routes
+router.use("/users", userRoutes)
 
 module.exports = router;
